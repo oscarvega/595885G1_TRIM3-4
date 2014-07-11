a631ee51145b45dfde759aa65010d85e2db6eda2
hemoreno33 <hemoreno33@misena.edu.co>
2014-07-10 23:37:47 -0500

    tabulación de los querys

--- MySQL/ejemploClase1.sql
+++ MySQL/ejemploClase1.sql
@@ -1,12 +1,12 @@
 create database a;
-create table a.persona(
-tipo_documento varchar(20),
-numero_documento varchar(20),
-primer_nombre varchar(40),
-segundo_nombre varchar(40),
-primer_apellido varchar(40),
-segundo_apellido varchar(40),
-primary key (tipo_documento,numero_documento)
+create table a.persona (
+    tipo_documento varchar(20),
+    numero_documento varchar(20),
+    primer_nombre varchar(40),
+    segundo_nombre varchar(40),
+    primer_apellido varchar(40),
+    segundo_apellido varchar(40),
+    primary key (tipo_documento , numero_documento)
 );
 
 INSERT INTO `a`.`persona`
@@ -25,7 +25,10 @@ VALUES
 'moreno');
 
 
-select * from a.persona;
+select 
+    *
+from
+    a.persona;
 alter table a.persona add column sexo int;
 alter table a.persona add column sexo varchar(10);
 alter table a.persona add column direccion varchar(20);
@@ -36,9 +39,25 @@ INSERT INTO `a`.`persona`
 VALUES
 ('masculino');
 
-UPDATE `a`.`persona` SET `sexo`='masculino' WHERE `tipo_documento`='cc' and`numero_documento`='51454';
-UPDATE `a`.`persona` SET `direccion`='kr 123 siempre viva' WHERE `tipo_documento`='cc' and`numero_documento`='51454';
-UPDATE `a`.`persona` SET `telefono`='2341233124' WHERE `tipo_documento`='cc' and`numero_documento`='51454';
+UPDATE `a`.`persona` 
+SET 
+    `sexo` = 'masculino'
+WHERE
+    `tipo_documento` = 'cc'
+        and `numero_documento` = '51454';
+UPDATE `a`.`persona` 
+SET 
+    `direccion` = 'kr 123 siempre viva'
+WHERE
+    `tipo_documento` = 'cc'
+        and `numero_documento` = '51454';
+
+UPDATE `a`.`persona` 
+SET 
+    `telefono` = '2341233124'
+WHERE
+    `tipo_documento` = 'cc'
+        and `numero_documento` = '51454';
 
 alter table a.persona drop column sexo;
 
