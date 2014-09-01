36ce8a1cd3235d1094c2ccf4f25d5e64032361d4
hemoreno33 <hemoreno33@misena.edu.co>
2014-09-01 17:57:16 -0500

    ejemplos asociacion

---
+++ JAVA/clases/EjemplosUML/src/edu/co/sena/uml/asociacion/unoamuchos/A.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.co.sena.uml.asociacion.unoamuchos;
+import java.util.Collections;
+
+
+/**
+ *
+ * @author Enrique Moreno
+ */
+public class A {
+    
+    private B b[];
+    public int numObjetos=0;
+    
+    public A() {
+        this.b= new B[10];
+    
+    }
+    
+    public static void agregarObjeto(B objetoAgregar){
+    
+    }
+    
+    
+    
+}
